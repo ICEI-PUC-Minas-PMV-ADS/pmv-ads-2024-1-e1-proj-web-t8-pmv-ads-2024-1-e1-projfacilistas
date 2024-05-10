42de2697e38dedb4e5545ee583bbf55629259971
unknown <brenos93@gmail.com>
2024-05-10 11:54:11 -0300

    feat: Adicionar funcionalidades de marcar um item já concluído
    Criar uma nova lista, através de um modelo pronto

---
+++ codigo-fonte/sistema/scripts/header.js
@@ -0,0 +1,20 @@
+const header = () => {
+  document.addEventListener("DOMContentLoaded", function () {
+    if (!userLogged || Object.keys(userLogged).length === 0) {
+      alert("Você não está logado. Por favor, faça login.");
+      window.location.href = "../login/index.html";
+    }
+  });
+
+  const logout = document.getElementsByClassName("btn-header");
+  logout[0].addEventListener("click", () => {
+    window.localStorage.setItem("userLogged", JSON.stringify({}));
+    window.location.href = "../paginaIncial/index.html";
+  });
+
+  const logo = document.getElementById("logo");
+  logo.addEventListener("click", () => {
+    window.location.href = "../paginaIncial/index.html";
+  });
+};
+header();
