d19d565f370e320d7a473827afc09add179e7d0c
Patrícia Ester Ferreira Rodrigues <100604597+patriciaferreirash@users.noreply.github.com>
2024-05-10 15:19:26 -0300

    Alterações no código de validação da página de login

--- codigo-fonte/login/js/script.js
+++ codigo-fonte/login/js/script.js
@@ -9,62 +9,65 @@ for (let input of inputs) {
     })
 }
 
+/*Validação do preenchimento do login e senha*/
+
+let form = document.getElementById('login-form');
+            form.addEventListener("submit", function(event) {
+            let inputs = document.getElementsByClassName('input-form');
+            for (let input of inputs) { 
+                if (input.value.trim() == ""){
+                    input.parentElement.classList.add('wrap-input-invalid');
+                } 
+            }
+            event.preventDefault();
+})
+
 
 /*Validação do login do formulário*/
 
+const allUsersString = window.localStorage.getItem("users");
+const allUsers = JSON.parse(allUsersString);
+ 
 function logar() {
-    var login = document.getElementById("login").value;
-    var senha = document.getElementById("senha").value;
+  var login = document.getElementById("login").value;
+  var senha = document.getElementById("senha").value;
+ 
+  let user;
+ 
+  allUsers.users.find((elem) => {
+    if (elem.email == login && elem.password == senha) {
+      user = elem;
+      window.localStorage.setItem("userLogged", JSON.stringify(elem));
+      location.href = "../sistema/index.html";
+    }
+  });
+ 
+  if (!user) {
+    alert("Usuário e login incorretos");
+  }
+}
+
+
+
+
+/*Validação simples do login do formulário*/
+
+/*function logar() {
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
 
     if(login == "admin@admin" && senha == "admin") {
         alert('sucesso')
-        location.href = "../../sistema/index.html"
+        location.href = "../sistema/index.html";
         } else {
         alert('Usuário e login incorretos')
     }
-};
+};*/
 
 
 
 
 
 
-/*teste*/
-/*Validação dos campos do formulário*/
 
-/*let form = document.getElementById('login-form');
-form.addEventListener("submit", function(event) {
-    let inputs = document.getElementsByClassName('input-form');
-    for (let input of inputs) { 
-        if (input.value.trim() == ""){
-            input.parentElement.classList.add('wrap-input-invalid');
-        } 
-    }
-    event.preventDefault();
-});*/
 
-/*if(login.value == "" || senha.value == "") {
-    let form = document.getElementById('login-form');
-    form.addEventListener("submit", function(event) {
-        let inputs = document.getElementsByClassName('input-form');
-        for (let input of inputs) { 
-            if (input.value.trim() == ""){
-                input.parentElement.classList.add('wrap-input-invalid');
-            } 
-        }
-        event.preventDefault();
-        });
-
-} else {
-    function logar() {
-    var login = document.getElementById("login").value;
-    var senha = document.getElementById("senha").value;
-
-    if(login == "admin@admin" && senha == "admin") {
-        alert('sucesso')
-        location.href = "../../sistema/index.html"
-        } else {
-        alert('Usuário e login incorretos')
-    }
-};
-}*/
