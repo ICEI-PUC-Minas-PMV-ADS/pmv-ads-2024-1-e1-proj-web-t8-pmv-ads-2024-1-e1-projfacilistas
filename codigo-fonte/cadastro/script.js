cb846ff06fc89011107b43df44c26be6811e12e4
adriansodr3 <adriansodree@gmail.com>
2024-05-12 17:34:32 -0300

    ajustando cadastro de usuário

--- codigo-fonte/cadastro/script.js
+++ codigo-fonte/cadastro/script.js
@@ -1,27 +1,43 @@
 let inputs = document.getElementsByClassName("input-form");
-        for(let input of inputs) {
-            input.addEventListener("blur", function(){
-                if(input.value.trim() != ""){
-                    input.classList.add("has-val");
-                } else {
-                    input.classList.remove("has-val");
-                }
-        })
-        }
+for (let input of inputs) {
+  input.addEventListener("blur", function () {
+    if (input.value.trim() != "") {
+      input.classList.add("has-val");
+    } else {
+      input.classList.remove("has-val");
+    }
+  });
+}
 
-const createUser = () => {
-    const allUsersString = window.localStorage.getItem("users");
-    const allUsers = JSON.parse(allUsersString);
-   
-    const users = allUsers;
-    users.users.push({
-      id: "3",
-      name: document.getElementsByName("name"),
-      email: document.getElementsByName("email"),
-      password: document.getElementsByName("password"),
-    });
-    window.localStorage.setItem("users", JSON.stringify(users));
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+
+  const nome = document.querySelector("#nome").value;
+  const email = document.querySelector("#email").value;
+  const senha = document.querySelector("#senha").value;
+
+  if (!nome | !email | !senha) {
+    alert("Todos os dados devem ser preenchidos");
+    return;
+  }
+
+  const allUsers = window.localStorage.getItem("contas");
+  const allUsersFormated = JSON.parse(allUsers);
+
+  const newUser = {
+    email: email,
+    nome: nome,
+    senha: senha,
   };
+
+  allUsersFormated.push(newUser);
+
+  window.localStorage.setItem("contas", JSON.stringify(allUsersFormated));
+
+  window.location.href = "../login/login.html";
+});
 
 /*   const createUser = () => {
     const allUsersString = window.localStorage.getItem("users");
